8fd4f7411dcca9dca674f55674e9739c2cd9f310
Foxelou <97977229+Foxelou@users.noreply.github.com>
2025-11-25 10:33:23 +0100

    Fix code style and add Pint configuration with pint.json

--- routes/web.php
+++ routes/web.php
@@ -54,4 +54,4 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
