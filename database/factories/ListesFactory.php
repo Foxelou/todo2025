b7ad172b9780b60036135fbb57edc68d0983895c
Foxelou <97977229+Foxelou@users.noreply.github.com>
2025-12-08 15:08:13 +0100

    Intégration de l'environnement de tests et de la couverture

---
+++ database/factories/ListesFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Listes>
+ */
+class ListesFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'titre' => fake()->words(2, true), // par ex. "Maison", "Bureau perso"
+        ];
+    }
+}
